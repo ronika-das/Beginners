717d092de2fcc37a856651e6ade6018b080b59b1
Ronika Das <ronikadas@gmail.com>
2018-02-08 12:52:30 +0530

    conio stuff

--- CPP/CPP_fibonacci.cpp
+++ CPP/CPP_fibonacci.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<conio.h>
 using namespace std;
 int main()
 {
@@ -15,5 +16,6 @@ next = first + second;
 first = second;
 second = next;
 }
-    return 0;
+    	getch();
+	return 0;
 }
